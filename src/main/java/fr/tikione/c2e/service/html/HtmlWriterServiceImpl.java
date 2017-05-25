cdd09701c5147f7e87f2a3b71d8dfdfa35b51e37
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2017-05-25 17:42:28 +0200

    (refac) introduce HTML template

--- src/main/java/fr/tikione/c2e/service/html/HtmlWriterServiceImpl.java
+++ src/main/java/fr/tikione/c2e/service/html/HtmlWriterServiceImpl.java
@@ -6,6 +6,7 @@ import fr.tikione.c2e.model.web.Magazine;
 import fr.tikione.c2e.model.web.TocCategory;
 import fr.tikione.c2e.model.web.TocItem;
 import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,6 +16,7 @@ import java.io.Writer;
 import java.util.Date;
 
 import static fr.tikione.c2e.Main.VERSION;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class HtmlWriterServiceImpl implements HtmlWriterService {
     
@@ -29,29 +31,18 @@ public class HtmlWriterServiceImpl implements HtmlWriterService {
         if (file.exists()) {
             throw new IOException("cannot write over file " + file.getAbsolutePath());
         }
-        String css = "";
-        String js = "";
-        StringBuilder buff = new StringBuilder();
-        buff.append("<!DOCTYPE html>\n");
-        buff.append("<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"fr\">\n");
-        buff.append("<!-- ").append(new Date()).append(" -->\n");
-        buff.append("<!-- File generated thanks to TikiOne C2E ").append(VERSION).append(" - https://github.com/jonathanlermitage/tikione-c2e -->\n");
-        buff.append("<!-- This file is for private use, please do NOT share on DDL ou P2P networks -->\n");
-        buff.append("<!-- Your CanardPC username is '").append(magazine.getLogin()).append("' -->\n");
-        buff.append("<head>\n");
-        buff.append("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n");
-        buff.append("  <meta charset=\"utf-8\"/>\n");
-        buff.append("  <title>").append("CPC n°").append(magazine.getNumber()).append("</title>\n");
-        buff.append("  <style>\n").append(css).append("\n  </style>\n");
-        buff.append("  <script>\n").append(js).append("\n  </script>\n");
-        buff.append("</head>\n\n");
-        buff.append("<body>\n\n");
+        String header = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("tmpl/html-export/header.html"), UTF_8)
+                .replace("$$login$$", magazine.getLogin())
+                .replace("$$version$$", VERSION)
+                .replace("$$timestamp$$", new Date().toString())
+                .replace("$$mag_number$$", Integer.toString(magazine.getNumber()));
+        String footer = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("tmpl/html-export/footer.html"), UTF_8);
         
         // TODO escape HTML contents!!
         // TODO add a tiny PayPal donation link at end of document
         // TODO a JS/CSS selector to switch to night-mode
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            bw.write(buff.toString());
+            bw.write(header);
             for (TocCategory category : magazine.getToc()) {
                 bw.write("<h2 class=\"category-title\">" + category.getTitle() + "</h2>\n\n");
                 for (TocItem tocItem : category.getItems()) {
@@ -59,7 +50,7 @@ public class HtmlWriterServiceImpl implements HtmlWriterService {
                     tocItem.getArticles().forEach(article -> writeArticle(bw, article));
                 }
             }
-            bw.write("</body>\n</html>\n");
+            bw.write(footer);
         }
     }
     
