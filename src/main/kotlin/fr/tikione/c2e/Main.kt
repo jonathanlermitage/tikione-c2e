da6dd161d5a7f6fcadf83c1e9efc4f1d45fec1a6
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2017-10-20 21:37:23 +0200

    (doc) bump version 1.3.6

--- src/main/kotlin/fr/tikione/c2e/Main.kt
+++ src/main/kotlin/fr/tikione/c2e/Main.kt
@@ -17,7 +17,7 @@ object Main {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
     var DEBUG = false
-    val VERSION = "1.3.5"
+    val VERSION = "1.3.6"
     private val VERSION_URL = "https://raw.githubusercontent.com/jonathanlermitage/tikione-c2e/master/uc/latest_version.txt"
     private val PAUSE_BETWEEN_MAG_DL = 30L
 
